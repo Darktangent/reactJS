2a0e5f68c9b28ef4f7781f27b651a731390c8fe3
Rohan Ganguly <rohan.ganguly@snhu.edu>
2018-06-23 09:45:46 +0000

    adding features to the src/app.js so that element in the DOM can be re-rendered

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,72 +5,83 @@ console.log('App.js is running');
 //JSX Javascript XML
 var app = {
     title: 'Indecision APP',
-    subtitle: 'Put your life in the hands of my App'
+    subtitle: 'Put your life in the hands of my App',
+    options: ['One', "Two"]
 };
-var template = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        'App Name: ',
-        app.title.toLocaleUpperCase()
-    ),
-    React.createElement(
-        'p',
-        null,
-        app.subtitle
-    ),
-    React.createElement(
-        'ol',
+
+var onFormSubmit = function onFormSubmit(e) {
+    e.preventDefault();
+
+    var option = e.target.elements.option.value;
+
+    if (option) {
+        app.options.push(option);
+        e.target.elements.option.value = '';
+        rederFunction();
+    }
+};
+var onRemoveAll = function onRemoveAll() {
+
+    app.options = [];
+    rederFunction();
+};
+var appRoute = document.getElementById('app');
+
+var rederFunction = function rederFunction() {
+    var template = React.createElement(
+        'div',
         null,
         React.createElement(
-            'li',
+            'h1',
             null,
-            'Item one'
+            'App Name: ',
+            app.title.toLocaleUpperCase()
+        ),
+        app.subtitle && React.createElement(
+            'p',
+            null,
+            app.subtitle
         ),
         React.createElement(
-            'li',
+            'p',
             null,
-            'Item two'
+            app.options.length > 0 ? 'Here are the options' : 'No options'
+        ),
+        React.createElement(
+            'p',
+            null,
+            app.options.length
+        ),
+        React.createElement(
+            'button',
+            { onClick: onRemoveAll },
+            'Remove all'
+        ),
+        React.createElement(
+            'ol',
+            null,
+            React.createElement(
+                'li',
+                null,
+                'Item one'
+            ),
+            React.createElement(
+                'li',
+                null,
+                'Item two'
+            )
+        ),
+        React.createElement(
+            'form',
+            { onSubmit: onFormSubmit },
+            React.createElement('input', { type: 'text', name: 'option' }),
+            React.createElement(
+                'button',
+                null,
+                'Add Option'
+            )
         )
-    )
-);
-var user = {
-    name: 'Rohan',
-    age: 27,
-    location: 'Texas'
-
+    );
+    ReactDOM.render(template, appRoute);
 };
-var userName = 'Ron Ganguly';
-var userAge = 34;
-var userLocation = 'Texas';
-var templateTwo = React.createElement(
-    'div',
-    null,
-    React.createElement(
-        'h1',
-        null,
-        user.name.toUpperCase()
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Age:',
-        user.age
-    ),
-    React.createElement(
-        'p',
-        null,
-        'Location: ',
-        user.location
-    )
-);
-
-// var template = React.createElement(
-//     "p",
-//     { id: "someid" },
-//     "This is JSX from app.js"
-// );
-var appRoute = document.getElementById('app');
-ReactDOM.render(template, appRoute);
+rederFunction();
--- src/app.js
+++ src/app.js
@@ -3,43 +3,53 @@ console.log('App.js is running')
 
 
 //JSX Javascript XML
-var app={
+const app={
     title:'Indecision APP',
-    subtitle : 'Put your life in the hands of my App'
+    subtitle : 'Put your life in the hands of my App',
+    options : ['One',"Two"]
 }
-var template = (
-    <div>
-        <h1>App Name: {app.title.toLocaleUpperCase()}</h1>
-        <p>{app.subtitle}</p>
-        <ol>
-            <li>Item one</li>
-            <li>Item two</li>
 
-        </ol>
-    </div>
-);
-var user ={
-    name :'Rohan',
-    age:27,
-    location:'Texas'
+const onFormSubmit=(e)=>{
+    e.preventDefault();
 
+    const option = e.target.elements.option.value;
+
+    if(option){
+        app.options.push(option)
+        e.target.elements.option.value = ''
+        rederFunction();
+    }
 }
-let userName = 'Ron Ganguly'
-let userAge = 34
- let userLocation = 'Texas'
-let templateTwo=(
-    <div>
-        <h1>{user.name.toUpperCase()}</h1>
-        <p>Age:{user.age}</p>
-        <p>Location: {user.location}</p>
-    </div>
-);
+const onRemoveAll = ()=>{
+
+    app.options = []
+    rederFunction()
+}
+const appRoute= document.getElementById('app')
 
 
-// var template = React.createElement(
-//     "p",
-//     { id: "someid" },
-//     "This is JSX from app.js"
-// );
-var appRoute= document.getElementById('app')
-ReactDOM.render(template, appRoute);
+const rederFunction= ()=>{
+    const template = (
+        <div>
+            <h1>App Name: {app.title.toLocaleUpperCase()}</h1>
+           {app.subtitle && <p>{app.subtitle}</p>}
+            <p>{(app.options.length>0) ? 'Here are the options':'No options'}</p>
+           <p>{app.options.length}</p>
+           <button onClick={onRemoveAll}>Remove all</button>
+            <ol>
+                <li>Item one</li>
+                <li>Item two</li>
+    
+            </ol>
+            <form onSubmit={onFormSubmit}>
+            <input type="text" name='option' />
+            <button>Add Option</button>
+            
+            
+            
+            </form>
+        </div>
+    );
+    ReactDOM.render(template,appRoute)
+}
+rederFunction();
